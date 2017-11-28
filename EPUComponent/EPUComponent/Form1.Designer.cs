287c9937bf0eff47dc6f7a5ca501cefcfae48784
Boris Dov <megaloki@mail.ru>
2017-11-28 17:44:04 +0100

    - master comit

--- EPUComponent/EPUComponent/Form1.Designer.cs
+++ EPUComponent/EPUComponent/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(320, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tortoise2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 261);
+            this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
     }
 }
 
