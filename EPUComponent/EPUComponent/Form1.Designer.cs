54a00cc1516b2f42672ac04fd61419d66406d01d
Boris Dov <megaloki@mail.ru>
2017-11-28 15:31:10 +0100

    - fixed buton name.

--- EPUComponent/EPUComponent/Form1.Designer.cs
+++ EPUComponent/EPUComponent/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +41,21 @@
             this.button1.Text = "Feature1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // button5
             // 
-            this.button2.Location = new System.Drawing.Point(385, 73);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "feature3";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button5.Location = new System.Drawing.Point(385, 73);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "feature3";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 261);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -66,7 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
     }
 }
 
