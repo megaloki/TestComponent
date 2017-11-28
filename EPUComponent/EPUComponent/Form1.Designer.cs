5a54772dc63ead6aa40bb1e05bef3455b939fcd5
Boris Dov <megaloki@mail.ru>
2017-11-28 15:57:43 +0100

    - Fixed form issue

--- EPUComponent/EPUComponent/Form1.Designer.cs
+++ EPUComponent/EPUComponent/Form1.Designer.cs
@@ -29,12 +29,11 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 74);
+            this.button1.Location = new System.Drawing.Point(48, 74);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
